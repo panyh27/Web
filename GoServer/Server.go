7e201fb5763c42170ad4cbeae1e345d4ae3516e1
panyh27 <1095911685@qq.com>
2021-10-05 19:39:15 +0800

    增加antd

--- GoServer/Server.go
+++ GoServer/Server.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	files, _ := ioutil.ReadDir("./Files")
-	for _, file := range files {
-		fmt.Println(file.Name())
-	}
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
 		c.Header("Access-Control-Allow-Credentials", "true")
+		files, _ := ioutil.ReadDir("./Files")
+		fileNames := []string{}
+		for _, file := range files {
+			fileNames = append(fileNames, file.Name())
+		}
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": fileNames,
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
--- React/myapp/src/index.js
+++ React/myapp/src/index.js
@@ -3,59 +3,60 @@ import { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ReactMarkdown from 'react-markdown'
+import "antd/dist/antd.css";
+import { Layout, DatePicker, version } from "antd";
 import './index.css';
-
-
-function getData(fuck) {
-  fuck = "hello"
-  axios({
-    method: 'get',
-    url: 'http://localhost:8080/',
-    withCredentials: true
-  })
-    .then(function (response) {
-      var data = JSON.parse(response);
-      console.log(data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
+const { Header, Sider, Footer, Content } = Layout
+const classes = {
+  header: {
+    height: '100'
+  }
 }
-
 function Index() {
   const [data, setData] = useState("");
   const [markdown, setMarkdown] = useState('');
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     await axios({
-  //       method: 'get',
-  //       url: 'http://localhost:8080/',
-  //       withCredentials: true
-  //     })
-  //       .then(function (response) {
-  //         var data = JSON.stringify(response.data);
-  //         console.log(data);
-  //         setData(data);
-  //       })
-  //       .catch(function (error) {
-  //         console.log(error);
-  //       })
-  //   };
-  //   fetchData();
-  // }, []);
-
   useEffect(() => {
+    axios({
+      method: 'get',
+      url: 'http://localhost:8080/',
+      withCredentials: true
+    })
+      .then((response) => {
+        var data = JSON.stringify(response.data);
+        console.log(data);
+        setData(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+
     axios('MachineLearning.md')
       .then((response) => setMarkdown(response.data))
+      .catch((error) => {
+        console.log(error);
+      })
   }, []);
 
   return (
-    <div>
-      <h1>{data}</h1>
-      <ReactMarkdown>
-        {markdown}
-      </ReactMarkdown>
-    </div>
+
+    <Layout>
+      <Header className='header'>Header</Header>
+      <Layout className='body'>
+        <Sider>Sider</Sider>
+        <Content className='content'>
+          <ReactMarkdown className='markdown'>
+            {markdown}
+          </ReactMarkdown>
+        </Content>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
+
+    /* <h1>{data}</h1>
+
+    <DatePicker />
+     */
+
   );
 }
 ReactDOM.render(<Index />, document.getElementById('root'));
