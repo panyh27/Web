6e51e8f4cf688c915417f60053bf36514b1eb9b8
panyh27 <1095911685@qq.com>
2021-10-07 21:07:42 +0800

    动态渲染组件

--- GoServer/Server.go
+++ GoServer/Server.go
@@ -1,24 +1,69 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+type contents struct {
+	IsFloder bool        `json:"type"`
+	FileName string      `json:"name"`
+	Path     string      `json:"path"`
+	FileList []*contents `json: "children"`
+}
 
+func walk(path string, node *contents) {
+	// 列出当前目录下的所有目录、文件
+	files := listFiles(path)
+	// 遍历这些文件
+	for _, FileName := range files {
+		// 拼接全路径
+		fpath := filepath.Join(path, FileName)
+
+		// 构造文件结构
+		fio, _ := os.Lstat(fpath)
+		isDir := fio.IsDir()
+		// 将当前文件作为子节点添加到目录下
+		child := contents{isDir, FileName, fpath, []*contents{}}
+		node.FileList = append(node.FileList, &child)
+
+		// 如果遍历的当前文件是个目录，则进入该目录进行递归
+		if isDir {
+			walk(fpath, &child)
+		}
+	}
+	return
+}
+func listFiles(dirname string) []string {
+	f, _ := os.Open(dirname)
+	names, _ := f.Readdirnames(-1)
+	f.Close()
+	return names
+}
+
+func main() {
+	root := contents{true, "Files", "./Files", []*contents{}}
+
+	walk("./Files", &root)
+	data, _ := json.Marshal(root)
+
+	fmt.Printf("%s", data)
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
 		c.Header("Access-Control-Allow-Credentials", "true")
 		files, _ := ioutil.ReadDir("./Files")
-		fileNames := []string{}
+		FileNames := []string{}
 		for _, file := range files {
-			fileNames = append(fileNames, file.Name())
+			FileNames = append(FileNames, file.Name())
 		}
 		c.JSON(200, gin.H{
-			"message": fileNames,
+			"message": FileNames,
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
--- React/myapp/src/index.js
+++ React/myapp/src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ReactMarkdown from 'react-markdown'
 import "antd/dist/antd.css";
-import { Layout, DatePicker, version } from "antd";
+import { Layout, Menu, Breadcrumb } from "antd";
 import './index.css';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 const { Header, Sider, Footer, Content } = Layout
-const classes = {
-  header: {
-    height: '100'
-  }
-}
+
+
+const { SubMenu } = Menu
 function Index() {
-  const [data, setData] = useState("");
+  const [data, setData] = useState([]);
   const [markdown, setMarkdown] = useState('');
   useEffect(() => {
     axios({
@@ -22,9 +26,7 @@ function Index() {
       withCredentials: true
     })
       .then((response) => {
-        var data = JSON.stringify(response.data);
-        console.log(data);
-        setData(data);
+        setData(response.data.message);
       })
       .catch((error) => {
         console.log(error);
@@ -37,27 +39,52 @@ function Index() {
       })
   }, []);
 
+  let list=[]
+  data.map((item,index)=>{
+    list.push(
+      <Menu.Item key={index}><Link to={item}>{item}</Link></Menu.Item>
+      )
+  })
+  
+
   return (
 
     <Layout>
-      <Header className='header'>Header</Header>
+      <Header className='header'>
+        <div className="logo" />
+        <Menu mode="horizontal" defaultSelectedKeys={['blog']}>
+          <Menu.Item key="blog">博客</Menu.Item>
+          <Menu.Item key="photo">照片</Menu.Item>
+        </Menu>
+      </Header>
       <Layout className='body'>
-        <Sider>Sider</Sider>
-        <Content className='content'>
-          <ReactMarkdown className='markdown'>
-            {markdown}
-          </ReactMarkdown>
-        </Content>
+        <Sider width={200}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%', borderRight: 0 }}
+          >
+
+            {list}
+          </Menu>
+        </Sider>
+        <Layout style={{ padding: '0 0 0 24px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content className="content">
+            <ReactMarkdown>
+              {markdown}
+            </ReactMarkdown>
+          </Content>
+        </Layout>
       </Layout>
-      <Footer>Footer</Footer>
+      <Footer className="footer">Footer</Footer>
     </Layout>
-
-    /* <h1>{data}</h1>
-
-    <DatePicker />
-     */
-
   );
 }
-ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(<Router><Index /></Router>, document.getElementById('root'));
 
